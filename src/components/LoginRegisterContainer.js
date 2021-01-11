798c64dd991f4bd60fe9bd9d0ddc2d1a63e81a06
Kenza <kenza.boulisfane@gmail.com>
2021-01-10 21:45:18 -0500

    Add logic for toggling between login/register forms

---
+++ src/components/LoginRegisterContainer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+
+function LoginRegisterContainer () {
+
+    const [isLoginForm, setIsLoginForm] = React.useState(true);
+    const rightSideRef = React.useRef();
+    const displayText = isLoginForm ? 'Register' : 'Login';
+    const currentlyClicked = isLoginForm ? 'login' : 'register';
+
+    React.useLayoutEffect(() => {
+        rightSideRef.current.classList.add("right");
+    }, [rightSideRef])
+
+    const onClickRightSide = () => {
+        if (isLoginForm) {
+            rightSideRef.current.classList.remove("right");
+            rightSideRef.current.classList.add("left");
+        }
+        else {
+            rightSideRef.current.classList.remove("left");
+            rightSideRef.current.classList.add("right");
+        }
+        setIsLoginForm(!isLoginForm);
+    }
+
+
+    return (
+        <div className="login-register-main-container">
+            <div className="login">
+                <div className="container">
+                    {isLoginForm && (
+                        // <Login containerRef={(ref) => rightSideRef.current = ref} />
+                        <Login />
+
+                    )}
+                    {!isLoginForm && (
+                        // <Register containerRef={(ref) => rightSideRef.current = ref} />
+                        <Register />
+
+                    )}
+                </div>
+                <RightSide
+                    containerRef={rightSideRef}
+                    onClick={onClickRightSide}
+                    displayText={displayText}
+                    currentlyClicked={currentlyClicked}
+                />
+            </div>
+        </div>
+    )
+}
+
+const RightSide = (props) => {
+    return (
+        <div ref={props.containerRef}
+            className="right-side"
+            onClick={props.onClick}>
+            <div className="inner-container">
+                <div className="text">{props.displayText}</div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginRegisterContainer;
