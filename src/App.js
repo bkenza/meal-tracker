f180e537f09aff1aed94fc314c997c25e8156e7f
Kenza <kenza.boulisfane@gmail.com>
2021-01-11 19:19:58 -0500

    Require authentication to view app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import './style.scss';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './components/Home';
@@ -9,19 +9,27 @@ import NotFoundPage from './components/NotFoundPage';
 import LoginRegisterContainer from './components/LoginRegisterContainer';
 
 function App () {
+  const loggedIn = localStorage.getItem('username');
+
   return (
     <Router>
       <div className='main' >
         <Switch>
           <Route path='/' component={Home} exact={true}></Route>
           <Route path='/login-register' component={LoginRegisterContainer} />
-          <Route path='/dashboard' component={Dashboard} exact />
-          <Route path='/create-meal' component={CreateMeal} exact />
-          <Route path='/edit-meal/:id' component={EditMeal} />
+          <Route path='/dashboard' component={Dashboard} exact >
+            {!loggedIn ? <Redirect to='/login-register' /> : <Dashboard />}
+          </Route>
+          <Route path='/create-meal' component={CreateMeal} exact >
+            {!loggedIn ? <Redirect to='/login-register' /> : <CreateMeal />}
+          </Route>
+          <Route path='/edit-meal/:id' component={EditMeal} >
+            {!loggedIn ? <Redirect to='/login-register' /> : <EditMeal />}
+          </Route>
           <Route component={NotFoundPage} />
         </Switch>
       </div>
-    </Router>
+    </Router >
   );
 }
 
