b1e1f169c174d7c7515b6ea1d9a3d572d7373fe2
Kenza <kenza.boulisfane@gmail.com>
2021-01-23 23:12:22 -0500

    remove memory leaks

--- src/middleware/mealRequests.js
+++ src/middleware/mealRequests.js
@@ -15,7 +15,6 @@ export const getTotalMeals = async () => {
 }
 
 export const getAllMeals = async (username) => {
-    console.log(username);
     const GET_MEALS_ENDPOINT = `http://localhost:5000/meals/${username}`
     try {
         let response = await axios.get(GET_MEALS_ENDPOINT);
@@ -30,8 +29,6 @@ export const getAllMeals = async (username) => {
 }
 
 export const getMealById = async (id) => {
-    console.log('middleware');
-    console.log(id);
     const GET_MEAL_BY_ID_ENDPOINT = `http://localhost:5000/meals/${id.id}`;
 
     try {
@@ -62,7 +59,6 @@ export const addMeal = async (meal) => {
 }
 
 export const editMeal = async (meal) => {
-    console.log(meal)
     const EDIT_MEAL_ENDPOINT = `http://localhost:5000/meals/update/${meal.id.id}`;
     try {
         let response = await axios.post(EDIT_MEAL_ENDPOINT, meal);
